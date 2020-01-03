f7b054a3864055c86482380ac2fc934ae8784a68
SaM <admin@kaxtone.com>
2020-01-03 17:56:08 +0530

    Uploading RoboFriends.
    Done.

---
+++ precache-manifest.01dedfb98b326041ae77638ffe804d03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4983a89cc926ce59727a1722126e883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6078421f1b540b39b4e7",
+    "url": "/static/css/main.f056aa33.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6078421f1b540b39b4e7",
+    "url": "/static/js/main.972dd77f.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
